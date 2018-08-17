ee934bb0267f15f0027ccec5115224897552b3db
Your Name <you@example.com>
2018-08-17 09:19:53 +0530

    this is new one

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,12 +5,9 @@ class StudentsController < ApplicationController
 	end
 	def create
 	end
-
-
 	def show
-@stu = Student.new(name: params[:name],age: params[:age],gender: params[:gender],address: params[:address])
-  @stu.save
-@students=Student.all
+		@stu = Student.new(name: params[:name],age: params[:age],gender: params[:gender],address: params[:address])
+		@stu.save
+		@students=Student.all
 	end
-
 end
